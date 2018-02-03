59d969a4c54b048fc23e3fdccdcb075a35383746
Vladimir V. Vladimirov <mckeency@gmail.com>
2018-02-03 19:38:07 +0300

    Added table to main page.

--- mtable/models.py
+++ mtable/models.py
@@ -4,14 +4,43 @@ from django.utils import timezone
 # Create Class for cells of main table
 class CellValue(models.Model):
     #author = models.ForeignKey('auth.User')
-    #title = models.CharField(max_length=200)
-    #text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    site_name = 'MASTER-SAAO'
-    
+    #created_date = models.DateTimeField(default=timezone.now)
+    sitename = 'MASTER-Site-Name'
+
+    def  sun_alt(self):
+        return '90'
+
+    def __str__(self):
+        return self.sitename
+
+# MasterSite: MASTER-SAAO, MASTER-IAC etc
+class MasterSite(models.Model):
+    #author = models.ForeignKey('auth.User')
+    # created_date = models.DateTimeField(default=timezone.now)
+    sitename = models.CharField(max_length=20)
+    lat = '123'
+    lon = '456'
+    elev = '8000'
+    website = 'https://master-website.ru'
+
     def  sun_alt(self):
         return '45'
 
     def __str__(self):
-        return self.site_name
+        return self.sitename
+
+# Main Server: Main Server , Ebox server 
+class MainServer(models.Model):
+    #author = models.ForeignKey('auth.User')
+    # created_date = models.DateTimeField(default=timezone.now)
+    sitename = models.CharField(max_length=20)
+    isok = models.BooleanField()
+    iserr = models.BooleanField()
+    ipaddr = '127.0.0.1'
+
+    # Is ping UP?
+    def  is_ping(self):
+        return '45'
+
+    def __str__(self):
+        return self.sitename
